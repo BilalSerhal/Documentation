3d17f7619e7a612e0901f0181eb9edc353153011
BilalSerhal <bilal111.rg@gmail.com>
2022-12-06 13:25:57 +0200

    completed-Step_5.html + Step_5.js

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_5.js
@@ -0,0 +1,7 @@
+
+function multiplyBy()
+{
+        num1 = document.getElementById("first_number").value;
+        num2 = document.getElementById("second_number").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
