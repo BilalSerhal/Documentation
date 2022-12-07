7686241ef6acdbcf211d8573909b43a197201b06
BilalSerhal <bilal111.rg@gmail.com>
2022-12-07 09:28:27 +0200

    Step_1.html + Step_1.js

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_7.js
@@ -0,0 +1,5 @@
+var shoe_size= prompt("What's your shoe_size?");
+function multiplyBy(){
+    shoe_size = document.getElementById("shoe_size").value;
+    document.getElementById("result").innerHTML = shoe_size * 2;
+}
